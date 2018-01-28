1d450c3a7311dee1f9ab62375e713bafc2a3a7e7
Tunde Oladipupo <toladipupo3@gatech.edu>
2018-01-28 02:34:18 -0600

    changed ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 51473;
+        nRPCPort = 51573;
     }
 };
 static CBaseMainParams mainParams;
